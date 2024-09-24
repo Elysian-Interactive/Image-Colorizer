afc0013504a6bd654f729c04918a4a5c3d1da36c
Shane833 <williamsshane883@gmail.com>
2024-09-24 12:59:56 +0530

    Image Display and Cleanup Works

--- 2 - GUI/Image Colorizer.py
+++ 2 - GUI/Image Colorizer.py
@@ -1,10 +1,11 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QHBoxLayout, 
+from PyQt5.QtWidgets import (QApplication, QWidget, QLabel, QPushButton, QHBoxLayout, 
                              QVBoxLayout, QMessageBox, QFileDialog)
-from PyQt5.Gui import QPixmap
+from PyQt5.QtGui import QPixmap
 from Colorizer import Colorizer
 import cv2
 import os
+import glob
 
 # TODO : Use MessageBoxes
 
@@ -22,8 +23,8 @@ class ImageColorizer(QWidget):
         self.image_count = int(self.readImageCount())
         
         # Variables for the displayed color and gray images
-        self.gray_image = QPixmap()
-        self.color_image = QPixmap()
+        self.gray_image = QLabel(self)
+        self.color_image = QLabel(self)
         
         # Setting up the UI
         self.initializeUI()
@@ -46,7 +47,7 @@ class ImageColorizer(QWidget):
     def initializeUI(self):
         # Initial Window Setup
         self.setGeometry(100, 100, 1152, 648)
-        self.setWindowTitle("Empty Window")
+        self.setWindowTitle("Image Colorizer")
         
         # Creating the function to setup the buttons
         self.setupButtons()
@@ -56,7 +57,7 @@ class ImageColorizer(QWidget):
     def setupButtons(self):
         
         # Creating vertical layout to house all other layout
-        window_layout = QVBoxLayout()
+        window_layout = QHBoxLayout()
         
         # Various Buttons for different functions
         open_img_btn = QPushButton("Open Image", self)
@@ -68,16 +69,24 @@ class ImageColorizer(QWidget):
         save_img_btn = QPushButton("Save Image", self)
         save_img_btn.clicked.connect(self.saveImageFiles)
         
-        # Button Layout to store these buttons
-        button_layout = QHBoxLayout()
-        button_layout.setSpacing(100)
-        button_layout.addWidget(open_img_btn)
-        button_layout.addWidget(colorize_img_btn)
-        button_layout.addWidget(save_img_btn)
+        # Different Layouts
+        gray_image_layout = QVBoxLayout()
+        gray_image_layout.addWidget(self.gray_image)
+        gray_image_layout.addWidget(open_img_btn)
+        
+        colorize_layout = QVBoxLayout()
+        colorize_layout.addStretch()
+        colorize_layout.addWidget(colorize_img_btn)
+        colorize_layout.addStretch()
+        
+        color_image_layout = QVBoxLayout()
+        color_image_layout.addWidget(self.color_image)
+        color_image_layout.addWidget(save_img_btn)
         
         # Adding all other layouts in the main layout
-        window_layout.addStretch()
-        window_layout.addLayout(button_layout)
+        window_layout.addLayout(gray_image_layout)
+        window_layout.addLayout(colorize_layout)
+        window_layout.addLayout(color_image_layout)
         
         # Setting the main layout for the app
         self.setLayout(window_layout)
@@ -90,7 +99,14 @@ class ImageColorizer(QWidget):
         # Checking if the file is valid
         if file_name:
             self.gray_image_path = file_name[0]
+            self.displayGrayImage()
             print("Gray Image Succesfully opened")
+    
+    # Function to display the gray image on the screen
+    def displayGrayImage(self):
+        pixmap = QPixmap(self.gray_image_path)
+        pixmap = pixmap.scaled(400,300)
+        self.gray_image.setPixmap(pixmap)
     
     # Function to save the images after they have been converted
     def saveImageFiles(self):
@@ -99,20 +115,39 @@ class ImageColorizer(QWidget):
         
         # Checking if the filepath is valid and creating it
         if file_name:
-            cv2.imwrite(file_name[0], self.color_image)
-            print("Color image saved")
+            cv2.imwrite(file_name[0], self.generated_color_image)
+            # print("Color image saved")
     
     # Function where actual colorization takes place
     def colorizeImage(self):
         self.generated_color_image = self.colorizer.colorize(self.gray_image_path)
-        cv2.imwrite(str('Assets/' + str(self.image_count) + '.jpg'), self.generated_color_image)
+        # Saving the generated image temporarily
+        cv2.imwrite(str('Processed/' + str(self.image_count) + '.jpg'), self.generated_color_image)
+        # Displaying the image on the screen
+        self.displayColorImage()
+        # Incrementing the counter
         self.image_count += 1
-        print("Converted to color image")
+        #print("Converted to color image")
+    
+    # Function to display the color image on the screen
+    def displayColorImage(self):
+        image_path = str('Processed/' + str(self.image_count) + '.jpg')
+        pixmap = QPixmap(image_path)
+        pixmap = pixmap.scaled(400,300)
+        self.color_image.setPixmap(pixmap)
+        
     
     # Function to display the gray image after it has been opened
     # This function will delete the saved images generated in a session
     def clearData(self):
-        pass
+        pattern = "Processed/*.jpg"
+
+        # Use glob to find all files that match the pattern
+        files = glob.glob(pattern)
+
+        # Loop through the list of files and delete each one
+        for file in files:
+            os.remove(file)
     
     # Function to clean up and store some variables
     def closeEvent(self, event):
